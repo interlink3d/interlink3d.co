d518f5d69a4028a658f7abf598db641b1f4d00eb
interlink3d <interlink3d@gmail.com>
2018-01-20 19:14:33 -0600

    setup the initial angular framework

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactMeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
